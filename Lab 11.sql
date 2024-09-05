97c1d1a0bf08269f272955779a053d4759657e42
Shreeya1424 <140989885+Shreeya1424@users.noreply.github.com>
2024-09-05 19:30:03 +0530

    Add files via upload

---
+++ Lab 11.sql
@@ -0,0 +1,19 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# SQL Server Management Studio Solution File, Format Version 20.00
+VisualStudioVersion = 15.0.28307.421
+MinimumVisualStudioVersion = 10.0.40219.1
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Default|Default = Default|Default
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{24CB5BA9-2FF2-4386-A230-6DA08E486EE2}.Default|Default.ActiveCfg = Default
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {7421F178-69ED-4171-8CE7-F616CEE1CABE}
+	EndGlobalSection
+EndGlobal
